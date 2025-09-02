e70d9d06c991008bb02e037e5ad516e86f95f347
Yash Chopra <ajayparmar@Yash-Chopras-MacBook.local>
2025-09-03 01:59:28 +0530

    Add OTP copy paste feature, Fix Bug, updated readme File

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # React-OTP-App
+This is a simple React App for creating a UI for OTP component, having basic frontend features of OTP inputs
--- my-react-app/src/components/OTP.jsx
+++ my-react-app/src/components/OTP.jsx
@@ -1,29 +1,71 @@
 import React, { useEffect, useRef, useState } from "react";
 
+// We are creating a OTP component which accepts a prop of otpLength
+// otpLength is defaulted to 4
 function Otp({ otpLength = 4 }) {
   const [otpFields, setOtpFields] = useState(new Array(otpLength).fill(""));
   const ref = useRef([]);
 
-  function handleKeyDown(e, index){
+  function handleKeyDown(e, index) {
     const key = e.key;
-    if(key === 'ArrowRight'){
-        if(index < otpFields.length - 1) ref.current[index + 1].focus();
-           // return;
+    if (key === "ArrowRight") {
+      if (index < otpFields.length - 1) ref.current[index + 1].focus();
     }
 
-    if(key === 'ArrowLeft'){
-        if(index > 0) ref.current[index - 1].focus();
-          //  return;
+    if (key === "ArrowLeft") {
+      if (index > 0) ref.current[index - 1].focus();
     }
 
+    //Make the copy of OPT array
+    const copyOtpFields = [...otpFields];
+
+    if (key === "Backspace") {
+      copyOtpFields[index] = "";
+      setOtpFields(copyOtpFields);
+
+      if (index > 0) ref.current[index - 1].focus();
+    }
+
+    // Only single digit entries would be allowed
+    if (!/^\d$/.test(key)) {
+      // We check if the key pressed is anything else than a digit
+      // If that is true then we simply return as this character is invalid
+      return;
+    }
+    copyOtpFields[index] = key;
+    setOtpFields(copyOtpFields);
+
+    if (index < otpFields.length - 1) ref.current[index + 1].focus();
   }
 
-  useEffect(() =>{
+  function handlePaste(e) {
+    e.preventDefault();
+
+    const pastedDated = e.clipboardData.getData("text");
+    const digits = pastedDated.match(/\d/g);
+    //The regex above will automatically filter out the digits from string
+
+    if (!digits) return;
+
+    const nextOtp = otpFields.slice();
+
+    for (let i = 0; i < digits.length; ++i) {
+      nextOtp[i] = digits[i];
+    }
+
+    setOtpFields(nextOtp);
+
+    const nextFocus = digits.length < otpLength ? digits.length : otpLength;
+    ref.current[nextFocus].focus();
+  }
+
+  useEffect(() => {
     ref.current[0]?.focus();
   }, []);
-  
+
   return (
     <div className="container">
+      {console.log("otpFields= ", otpFields)}
       {otpFields.map((value, index) => (
         <input
           key={index}
@@ -31,8 +73,9 @@ function Otp({ otpLength = 4 }) {
           value={value}
           maxLength={1}
           className="otp-input"
-          ref={(currentInput) =>(ref.current[index]= currentInput)}
+          ref={(currentInput) => (ref.current[index] = currentInput)}
           onKeyDown={(e) => handleKeyDown(e, index)}
+          onPaste={handlePaste}
         />
       ))}
     </div>
